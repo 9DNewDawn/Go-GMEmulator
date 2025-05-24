0d2df8c271bd0999543e9582c116cf7e04b0cd6f
Renaud <renaudgdumont@gmail.com>
2025-05-24 11:59:19 -0400

    Able to read MSG_SYSTEM_TIME_RES

--- MSG_OPER_TOOL.go
+++ MSG_OPER_TOOL.go
@@ -4,12 +4,19 @@ type GmsHeader struct {
 	IKey     int32    // 4 bytes
 	CMessage uint8    // 1 byte
 	UITime   uint32   // 4 bytes
-	CGMName  [15]byte // 13 bytes
+	CGMName  [13]byte // 13 bytes
 }
 
 type MSG_SYSTEM_TIME_REQ struct {
 	Header GmsHeader
 }
 
+type MSG_SYSTEM_TIME_RES struct {
+	Header       GmsHeader
+	UITime       uint32
+	UServerIndex uint16 // Server index
+}
+
 const MSG_KEY = 1003
 const MSG_SYSTEM_TIME_REQ_NUM = 11
+const MSG_SYSTEM_TIME_RES_NUM = 12
--- test.go
+++ test.go
@@ -22,7 +22,7 @@ func main() {
 			IKey:     MSG_KEY,
 			CMessage: MSG_SYSTEM_TIME_REQ_NUM,
 			UITime:   0,
-			CGMName:  [15]byte{'f', 'i', 'r', 'e', 'f', 'o', 'x', 0, 0, 0, 0, 0, 0},
+			CGMName:  [13]byte{'f', 'i', 'r', 'e', 'f', 'o', 'x', 0, 0, 0, 0, 0, 0},
 		},
 	}
 
@@ -62,11 +62,81 @@ func main() {
 	fmt.Println("Data sent successfully!")
 
 	// Read the response
-	response := make([]byte, 1024)
-	n, err := conn.Read(response)
+	// Buffer management variables (simulate the C++ logic)
+	const MAX_OF_RECV_BUFFER = 4096
+	recvBuffer := make([]byte, MAX_OF_RECV_BUFFER)
+	curStartPos := 0
+	curEndPos := 0
+
+	// Read data into buffer
+	n, err := conn.Read(recvBuffer[curEndPos:])
 	if err != nil {
 		fmt.Println("Error reading response:", err)
 		return
 	}
-	fmt.Println("Response received:", response[:n])
+	curEndPos += n
+
+	// Try to extract a packet
+	var packetLen uint16
+	if curStartPos < curEndPos-2 {
+		packetLen = binary.LittleEndian.Uint16(recvBuffer[curStartPos : curStartPos+2])
+		if packetLen > MAX_OF_RECV_BUFFER {
+			fmt.Println("[RecvBuffer] Exception: Packet Length Overflow")
+			return
+		}
+		if curEndPos-curStartPos >= int(packetLen)+2 {
+			packet := recvBuffer[curStartPos+2 : curStartPos+2+int(packetLen)]
+			curStartPos += int(packetLen) + 2
+
+			// Parse MSG_KEY and CMessage
+			if len(packet) >= 5 {
+				msgKey := int(binary.LittleEndian.Uint32(packet[0:4]))
+				if msgKey == MSG_KEY {
+					cMessage := packet[4]
+					switch cMessage {
+					// Add your case handlers here
+					case MSG_SYSTEM_TIME_RES_NUM:
+						// Cast packet to MSG_SYSTEM_TIME_RES
+						expectedSize := binary.Size(MSG_SYSTEM_TIME_RES{})
+						if len(packet) >= binary.Size(MSG_SYSTEM_TIME_RES{}) {
+							var res MSG_SYSTEM_TIME_RES
+							buf := bytes.NewReader(packet)
+							err := binary.Read(buf, binary.LittleEndian, &res)
+							if err != nil {
+								fmt.Println("Failed to parse MSG_SYSTEM_TIME_RES:", err)
+								break
+							}
+							fmt.Printf("Received MSG_SYSTEM_TIME_RES: ServerIndex=%d, Time=%d\n", res.UServerIndex, res.UITime)
+							// You can add logic here to compare with local time and set time gap if needed
+						} else {
+							fmt.Printf("Packet too short: got %d bytes, expected at least %d bytes\n", len(packet), expectedSize)
+						}
+						break
+					default:
+						fmt.Printf("Unknown CMessage: %d\n", cMessage)
+					}
+				} else {
+					fmt.Printf("MSG_KEY mismatch: got %d, expected %d\n", msgKey, MSG_KEY)
+				}
+			} else {
+				fmt.Println("Packet too short to parse MSG_KEY and CMessage")
+			}
+			fmt.Println("Packet received:", packet)
+			return
+		}
+		// Not enough data for a full packet, move remaining data to start
+		if curStartPos != 0 {
+			fmt.Println("Partial packet received, shifting buffer")
+			copy(recvBuffer[0:], recvBuffer[curStartPos:curEndPos])
+			curEndPos -= curStartPos
+			curStartPos = 0
+			for i := curEndPos; i < MAX_OF_RECV_BUFFER; i++ {
+				recvBuffer[i] = 0
+			}
+		} else {
+			fmt.Println("Partial packet received, waiting for more data")
+		}
+	} else {
+		fmt.Println("Not enough data to determine packet length")
+	}
 }
